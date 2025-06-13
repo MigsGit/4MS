d811691e77a88a0a8333efdcf210210144c6328c
MigsGit <miguellgsp12345@gmail.com>
2025-06-13 10:36:46 +0800

    Environment | Save PMI Approver Decision

---
+++ database/migrations/2025_06_10_102835_create_environments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEnvironmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('environments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('ecrs_id')->references('id')->on('ecrs')->comment ='Ecr Id';
+            $table->string('approval_status')->default('PB')->comment('PB - Prepared by | CB - Checked by | AP- Approved by');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('environments');
+    }
+}
