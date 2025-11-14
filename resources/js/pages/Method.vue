77962a703da6e46cb918f93dc58424bf3b42e898
MigsGit <miguellgsp12345@gmail.com>
2025-11-15 07:22:21 +0800

    Method | Show PMI Approvers

--- resources/js/pages/Method.vue
+++ resources/js/pages/Method.vue
@@ -306,7 +306,7 @@
                             </div>
                         </div>
             </div>
-            <div class="row mt-3" v-show="isModal === 'View' && currentStatus === 'PMIAPP'" >
+            <div class="row mt-3" v-show="isModal === 'View' && currentStatus === 'PMIAPP' || currentStatus === 'OK'">
                 <div class="card mb-2">
                         <h5 class="mb-0">
                             <button id="" class="btn btn-link collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapsePmiInternalApprovalSummary" aria-expanded="true" aria-controls="collapsePmiInternalApprovalSummary">
